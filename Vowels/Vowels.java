fc4a4965eb319c12d7d199e1cb9770400dcee349
rkim <rkim@nevada.unr.edu>
2021-05-21 08:43:30 -0700

    Added digit check to vowels and consonants class.

--- Vowels/Vowels.java
+++ Vowels/Vowels.java
@@ -7,7 +7,7 @@ class Vowels {
         for(int i = 0; i < lowercase.length(); i++) {
             char c = lowercase.charAt(i);
             System.out.println(c);
-            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || !c.isDigit()) {
                 vowelCount++;
             }
         }
@@ -16,9 +16,9 @@ class Vowels {
 
     public static void main(String[] args) {
         /*
-1. Create Vowels class in JAVA then do the following:
-- Ask User to enter a sentence or create a String variable to assign a sentence to it
-- Count all the vowels in a sentence and display the count
+        1. Create Vowels class in JAVA then do the following:
+        - Ask User to enter a sentence or create a String variable to assign a sentence to it
+        - Count all the vowels in a sentence and display the count
         */
         String userInput;
         // prompt a user input
